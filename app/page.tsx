c44caf3c2753fef6c19836e5592d9a8b5989c559
Mohammed <m.mochoui@gmail.com>
2025-01-25 16:43:49 +0100

    Added input fields'

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import styles from "./page.module.css";
+import Form from 'next/form'
 
 import { useState, useEffect } from "react";
 
@@ -23,6 +24,14 @@ type Travel = {
 
 export default function Home() {
   const [data, setData] = useState<Travel[] | null>(null);
+  const [submitData, setSubmitData] = useState<Travel>({
+    adults: 0,
+    children: 0,
+    children_no_bed: 0,
+    babies: 0,
+    room_type: RoomType.EENPERSOONS,
+    non_eu_travelers: 0
+  });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -37,23 +46,24 @@ export default function Home() {
     fetchData();
   }, [data])
 
-  const createTravel = async (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+  const createTravel = async (e: React.FormEvent) => {
     e.preventDefault();
     await fetch("https://ut-mz6c.onrender.com/", {
       method: "post",
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({
-        adults: 1,
-        babies: 1,
-        children: 1,
-        children_no_bed: 0,
-        non_eu_travelers: 0,
-        room_type: RoomType.EENPERSOONS
-      } as Travel)
+      body: JSON.stringify(submitData)
     }
     );
+    setSubmitData({
+      adults: 0,
+      children: 0,
+      children_no_bed: 0,
+      babies: 0,
+      room_type: RoomType.EENPERSOONS,
+      non_eu_travelers: 0
+    });
   }
 
   return (
@@ -68,6 +78,34 @@ export default function Home() {
             height={38}
             priority
           />
+          <Form action="">
+            <div>
+              <label htmlFor="room_type">Room Type:</label>
+              <select id="room_type" name="room_type">
+                {Object.keys(RoomType).filter((v) => isNaN(Number(v))).map(k => <option value={k}>{k}</option>)}
+              </select>
+            </div>
+            <div>
+              <label htmlFor="adults">Adults:</label>
+              <input type="number" id="adults" value={submitData.adults} onChange={(e) => setSubmitData({ ...submitData, adults: Number(e.target.value) })} />
+            </div>
+            <div>
+              <label htmlFor="children">Children:</label>
+              <input type="number" id="children" value={submitData.children} onChange={(e) => setSubmitData({ ...submitData, children: Number(e.target.value) })} />
+            </div>
+            <div>
+              <label htmlFor="babies">Babies:</label>
+              <input type="number" id="babies" value={submitData.babies} onChange={(e) => setSubmitData({ ...submitData, babies: Number(e.target.value) })} />
+            </div>
+            <div>
+              <label htmlFor="children_no_bed">Children no bed:</label>
+              <input type="number" id="children_no_bed" value={submitData.children_no_bed} onChange={(e) => setSubmitData({ ...submitData, children_no_bed: Number(e.target.value) })} />
+            </div>
+            <div>
+              <label htmlFor="non_eu_travelers">Non eu Travelers:</label>
+              <input type="number" id="non_eu_travelers" value={submitData.non_eu_travelers} onChange={(e) => setSubmitData({ ...submitData, non_eu_travelers: Number(e.target.value) })} />
+            </div>
+          </Form>
           <ol>
             {data?.map((t, index) => {
               return <li key={index}>Room of type {t.room_type} with {t.adults} adults, {t.babies} babies, {t.children} children and {t.children_no_bed} without a bed, {t.non_eu_travelers} non eu travelers.</li>
